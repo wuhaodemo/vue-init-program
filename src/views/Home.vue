4969244a6a1c94fb0b8bbc774751426ab2a8fd02
吴浩 <1057661454@qq.com>
2020-12-17 10:02:13 +0800

    feat:dev-wh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,60 @@
 <template>
   <div class="home1">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="show = !show">Toggle</button>
+    <transition v-on:before-enter="beforeEnter" v-on:enter="enter" v-on:leave="leave" v-bind:css="false">
+      <!-- <img :src="img" alt="" v-if="show" /> -->
+      <p v-if="show">demo</p>
+    </transition>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import animations from 'create-keyframe-animation';
 export default {
-  name: "Home",
+  name: 'Home',
+  data() {
+    return {
+      show: false,
+      img: require('@/assets/logo.png')
+    };
+  },
+  methods: {
+    beforeEnter: function (el) {
+      el.style.opacity = 0;
+      el.style.transformOrigin = 'left';
+      animations.registerAnimation({
+        name: 'move',
+        // the actual array of animation changes
+        animation: {
+          0: {
+            transform: `translate3d(900px,900px,0) scale(1)`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        },
+        // optional presets for when actually running the animation
+        presets: {
+          duration: 1000,
+          // easing: 'linear',
+          delay: 500
+        }
+      });
+      animations.runAnimation(el, 'move', function () {
+        console.log('done');
+        // callback gets called when its done
+      });
+      console.log('dd');
+    },
+    enter: function (el, done) {
+      console.log(el, done);
+    },
+    leave: function (el, done) {
+      console.log(el, done);
+    }
+  }
 };
 </script>
